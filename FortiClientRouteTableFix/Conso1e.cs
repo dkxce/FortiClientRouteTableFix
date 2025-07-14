75c44e91162c2f36d46d56893e777835c0c3b209
dkxce <dkxce@mail.ru>
2025-07-14 16:51:44 +0300

    ver1

---
+++ FortiClientRouteTableFix/Conso1e.cs
@@ -0,0 +1,247 @@
+﻿//
+// C# (.Net Framework) Windows Win7+
+// System.Conso1e
+// v 0.2, 18.07.2023
+// https://github.com/dkxce
+// en,ru,1251,utf-8
+//
+
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace System
+{
+    /// <summary>
+    ///     Console Class to ReadKey / ReadLine with Read Timeout
+    /// </summary>
+    public class Conso1e
+    {
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        private const int SW_HIDE = 0;
+        private const int SW_SHOW = 5;
+
+        private static Thread inputThread;
+        private static AutoResetEvent getInput, gotInput;
+
+        private static string input;
+        private static bool retasis = false;
+        private static char pswchar = (char)0;
+
+        static Conso1e()
+        {
+            getInput = new AutoResetEvent(false);
+            gotInput = new AutoResetEvent(false);
+            inputThread = new Thread(reader);
+            inputThread.IsBackground = true;
+            inputThread.Start();
+        }
+
+        private static void reader()
+        {
+            while (true)
+            {
+                getInput.WaitOne();
+                input = readLine();
+                gotInput.Set();
+            }
+        }
+
+        /// <summary>
+        ///     Read Line
+        /// </summary>
+        /// <param name="timeOutMillisecs">Read TimeOut in milliseconds or 0 (zero)</param>
+        /// <param name="defaultValue">Default Return Value if Timeout Expired</param>
+        /// <param name="returnAsIs">Return Any Entered Value instead of defaultValue or not</param>
+        /// <param name="passwordChar">password char</param>
+        /// <returns></returns>
+        public static string ReadLine(int timeOutMillisecs = 0, string defaultValue = "", bool returnAsIs = false, char passwordChar = (char)0)
+        {
+            retasis = returnAsIs;
+            pswchar = passwordChar;
+            if (timeOutMillisecs <= 0) return readLine();
+            getInput.Set();
+            bool success = gotInput.WaitOne(timeOutMillisecs);
+            if (!success) Console.WriteLine();
+            return success ? input : (returnAsIs ? input : defaultValue);
+        }
+
+        /// <summary>
+        ///     Read Password Line
+        /// </summary>
+        /// <param name="timeOutMillisecs">Read TimeOut in milliseconds or 0 (zero)</param>
+        /// <param name="defaultValue">Default Return Value if Timeout Expired</param>
+        /// <param name="returnAsIs">Return Any Entered Value instead of defaultValue or not</param>
+        /// <returns></returns>
+        public static string ReadPassword(int timeOutMillisecs = 0, string defaultValue = "", bool returnAsIs = false)
+        {
+            return ReadLine(timeOutMillisecs, defaultValue, returnAsIs, '*');
+        }
+
+        /// <summary>
+        ///     Read Key
+        /// </summary>
+        /// <param name="timeOutMillisecs">Read TimeOut in milliseconds or 0 (zero)</param>
+        /// <param name="defaultKey">Default Return Key if Timeout Expired</param>
+        /// <returns></returns>
+        public static ConsoleKeyInfo ReadKey(int timeOutMillisecs = 0, ConsoleKey defaultKey = (ConsoleKey)0)
+        {
+            if (timeOutMillisecs <= 0) return Console.ReadKey();
+            ConsoleKeyInfo result = new ConsoleKeyInfo((char)defaultKey, (ConsoleKey)defaultKey, false, false, false);
+            Thread thr = new Thread(new ThreadStart(() => { result = Console.ReadKey(); }));
+            thr.Start();
+            thr.Join(timeOutMillisecs);
+            return result;
+        }
+
+        private static string readLine()
+        {
+            bool charIsNull = pswchar == (char)0;
+            if (charIsNull && !retasis) return Console.ReadLine();
+
+            string rln = string.Empty;
+            if (retasis) input = "";
+
+            ConsoleKey key;
+            do
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey(intercept: true);
+                key = keyInfo.Key;
+                if (key == ConsoleKey.Backspace && rln.Length > 0)
+                {
+                    Console.Write("\b \b");
+                    if (rln.Length > 0) rln = rln.Substring(0, rln.Length - 1);
+                }
+                else if (!char.IsControl(keyInfo.KeyChar))
+                {
+                    Console.Write(charIsNull ? keyInfo.KeyChar.ToString() : pswchar.ToString());
+                    rln += keyInfo.KeyChar;
+                };
+                if (retasis) input = rln;
+            } while (key != ConsoleKey.Enter);
+            Console.WriteLine();
+            return rln;
+        }
+
+        public static void AutoClose(int timeOutMillisecs, string text = "Done, Press Enter to close (autoclose in {0} seconds)...")
+        {
+            if (timeOutMillisecs <= 0) return;
+            int wait = timeOutMillisecs / 1000;
+            (new Thread(() => {
+                int top = Console.CursorTop;
+                while (wait > 0)
+                {
+                    Console.SetCursorPosition(0, top);
+                    Console.WriteLine(text, wait--);
+                    Thread.Sleep(1000);
+                };
+            })).Start();
+            Conso1e.ReadLine(timeOutMillisecs);
+            wait = 0;
+        }
+
+        public static void ShowConsole(int timeOutMillisecs = 0) { ShowConsole(IntPtr.Zero, timeOutMillisecs); }
+
+        public static void ShowConsole(IntPtr handle, int timeOutMillisecs = 0)
+        {
+            IntPtr cHandle = handle == IntPtr.Zero ? GetConsoleWindow() : handle;
+            if (cHandle == IntPtr.Zero) return;
+            if (timeOutMillisecs <= 0)
+                ShowWindow(cHandle, SW_SHOW);
+            else
+                (new Thread(new ThreadStart(() => {
+                    Thread.Sleep(timeOutMillisecs);
+                    ShowWindow(cHandle, SW_SHOW);
+                }))).Start();
+        }
+
+        public static void HideConsole(int timeOutMillisecs = 0) { HideConsole(IntPtr.Zero, timeOutMillisecs); }
+
+        public static void HideConsole(IntPtr handle, int timeOutMillisecs = 0)
+        {
+            IntPtr cHandle = handle == IntPtr.Zero ? GetConsoleWindow() : handle;
+            if (cHandle == IntPtr.Zero) return;
+            if (timeOutMillisecs <= 0)
+                ShowWindow(cHandle, SW_HIDE);
+            else
+                (new Thread(new ThreadStart(() => {
+                    Thread.Sleep(timeOutMillisecs);
+                    ShowWindow(cHandle, SW_HIDE);
+                }))).Start();
+        }
+    }
+
+    internal static class ProcWindows
+    {
+        [DllImport("user32.dll")]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool TerminateProcess(IntPtr hProcess, uint uExitCode);
+
+        public static void ShowLaunched()
+        {
+            string currProcName = System.IO.Path.GetFileNameWithoutExtension(System.Reflection.Assembly.GetEntryAssembly().Location);
+            Process[] procs = Process.GetProcessesByName(currProcName);
+            foreach (Process proc in procs)
+                if ((proc.MainWindowHandle != IntPtr.Zero) && (proc.MainWindowHandle != Process.GetCurrentProcess().MainWindowHandle))
+                {
+                    ShowWindow(proc.MainWindowHandle, 5);
+                    SetForegroundWindow(proc.MainWindowHandle);
+                    break;
+                };
+        }
+
+        public static void Kill()
+        {
+            string currProcName = System.IO.Path.GetFileNameWithoutExtension(System.Reflection.Assembly.GetEntryAssembly().Location);
+            Process[] procs = Process.GetProcessesByName(currProcName);
+            foreach (Process proc in procs)
+                if ((proc.MainWindowHandle != IntPtr.Zero) && (proc.MainWindowHandle != Process.GetCurrentProcess().MainWindowHandle))
+                {
+                    try { TerminateProcess(proc.Handle, 0); } catch { };
+                    try { proc.Kill(); } catch { };
+                    break;
+                };
+        }
+
+        public static void Kill(string procName)
+        {
+            Process[] procs = Process.GetProcessesByName(procName);
+            foreach (Process proc in procs)
+                if ((proc.MainWindowHandle != IntPtr.Zero) && (proc.MainWindowHandle != Process.GetCurrentProcess().MainWindowHandle))
+                {
+                    try { TerminateProcess(proc.Handle, 0); } catch { };
+                    try { proc.Kill(); } catch { };
+                    break;
+                };
+        }
+
+        public static void Kill(int procId)
+        {
+            try
+            {
+                Process proc = Process.GetProcessById(procId);
+                if ((proc.MainWindowHandle != IntPtr.Zero) && (proc.MainWindowHandle != Process.GetCurrentProcess().MainWindowHandle))
+                {
+                    try { TerminateProcess(proc.Handle, 0); } catch { };
+                    try { proc.Kill(); } catch { };
+                };
+            }
+            catch { };
+        }
+    }
+}
+
---
+++ FortiClientRouteTableFix/Program.cs
@@ -0,0 +1,404 @@
+﻿//
+// C# (.Net Framework) Windows Win7+
+// FortiClient Route IPTable Fixer
+// v 0.1, 14.07.2025
+// https://github.com/dkxce
+// en,ru,1251,utf-8
+//
+
+using System;
+using System.Collections.Generic;
+using System.Management;
+using System.Net.Sockets;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.IO;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+namespace FortiClientRouteTableFix
+{
+    internal class Program
+    {
+        static void Main(string[] args) => IPv4RouteChecker.StartChecking();
+    }
+
+    internal static class IPv4RouteChecker
+    {
+        private const string  defMetric    = "100";
+        private const string  vpnName      = "forti";
+        private const int     interval     = 90;
+        private static string defGateway   = "";
+        private static string defIndex     = "";
+        private static string defInterface = "";
+        private static bool   preventNextSleep = false;
+        private static List<string> ipList = new List<string>();       
+
+        static IPv4RouteChecker()
+        {
+            Dictionary<string,string> descs = new Dictionary<string,string>(); ;
+            string ip = "0.0.0.0";
+
+            try
+            {
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher(@"root\CIMV2", "SELECT * FROM Win32_NetworkAdapter");
+                foreach (ManagementObject queryObj in searcher.Get())
+                    descs.Add(queryObj["Index"].ToString(), queryObj["Description"].ToString());
+                searcher.Dispose();
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex);
+                Conso1e.ReadLine(5000);
+            };
+
+            try
+            {
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher(@"root\CIMV2", "SELECT * FROM Win32_IP4RouteTable");
+                foreach (ManagementObject queryObj in searcher.Get())
+                {
+                    try
+                    {
+                        string Caption = queryObj["Caption"].ToString();
+                        if (Caption != ip) continue;
+                        string Protocol = queryObj["Protocol"].ToString();
+                        //if (Protocol != "3") continue;
+
+                        string InterfaceIndex = queryObj["InterfaceIndex"].ToString();
+                        string Description = descs[InterfaceIndex];
+                        if (Description.ToLower().Contains("vpn") || Description.ToLower().Contains("adapter") || Description.ToLower().Contains(vpnName))
+                            continue;
+
+                        defGateway = queryObj["NextHop"].ToString();
+                        defIndex = InterfaceIndex;
+                        defInterface = Description;
+                        Console.WriteLine($"Direct Adapter: {Caption} -> {defGateway} `{defIndex} {defInterface}`");
+                    }
+                    catch { };
+                };
+                searcher.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Conso1e.ReadLine(5000);
+            };
+
+            try
+            {
+                string fName = System.Reflection.Assembly.GetEntryAssembly().Location;
+                fName = Path.Combine(Path.GetDirectoryName(fName), "IPLIST.txt");
+                ipList.AddRange(File.ReadAllLines(fName));
+                for (int i = ipList.Count - 1; i >= 0; i--)
+                {
+                    if (ipList[i].Contains(":"))
+                        ipList.RemoveAt(i);
+                    else
+                        ipList[i] = ipList[i].Trim().Split('/')[0];
+                };
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Conso1e.ReadLine(5000);
+            };
+
+            Conso1e.ReadLine(1000);
+        }
+
+        public static void StartChecking()
+        {
+            // CHECK LIST
+            if (ipList.Count == 0)
+            {
+                Console.WriteLine("NO IP LIST, past IP addresses into IPLIST.txt: 1 record per 1 line");
+                Conso1e.ReadLine(5000);
+                return;
+            };
+            Console.WriteLine($"Loaded {ipList.Count} IPs:");
+            foreach (string ip in ipList) Console.WriteLine($" - {ip}");
+            Conso1e.ReadLine(1000);
+
+            // LOOP
+            while (true)
+            {
+                LoopChecking(ipList);
+                PrintIP();
+
+
+                if (preventNextSleep)
+                {
+                    preventNextSleep = false;
+                    Console.WriteLine($"{DateTime.Now}: Wait {5} sec till {DateTime.Now.AddSeconds(5)}...");
+                    Thread.Sleep(5 * 1000);
+                }
+                else
+                {
+                    Console.WriteLine($"{DateTime.Now}: Wait {interval} sec till {DateTime.Now.AddSeconds(interval)}...");
+                    Thread.Sleep(interval * 1000);
+                };
+            };
+        }
+
+        private static void LoopChecking(List<string> ipList)
+        {
+            Console.WriteLine("");
+            Console.WriteLine($"{DateTime.Now}: ------------------------------------------------------ ");
+            Console.WriteLine($"{DateTime.Now}: Start Checking");
+       
+            foreach (string ip in ipList)
+            {
+                Console.WriteLine($"{DateTime.Now}:   Checking ip {ip}");
+                try
+                {
+                    IPEndPoint gatewayIp = QueryRoutingGateway(new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp), new IPEndPoint(IPAddress.Parse(ip), 0));
+                    string gatewayId = QueryRoutingGatewayId(gatewayIp.Address.ToString());
+                    string gatewayName = QueryRoutingGatewayName(gatewayId);
+
+                    bool ok = !gatewayName.ToLower().Contains(vpnName);
+                    string txt = ok ? "OK" : vpnName;                    
+                    Console.WriteLine($"{DateTime.Now}:     Route: {gatewayIp.Address} by {gatewayName} - {txt}");
+                    if (!ok)
+                        PassIP(ip);
+                    else
+                        NeedIP(ip);
+                }
+                catch (Exception ex)
+                {
+                    preventNextSleep = true;
+                    Console.WriteLine($"{DateTime.Now}:     Error: {ex}");
+                };
+            };
+
+            Console.WriteLine($"{DateTime.Now}: Checked");
+        }
+
+        private static IPEndPoint QueryRoutingGateway(Socket socket, IPEndPoint remoteEndPoint)
+        {
+            SocketAddress address = remoteEndPoint.Serialize();
+
+            byte[] remoteAddrBytes = new byte[address.Size];
+            for (int i = 0; i < address.Size; i++) remoteAddrBytes[i] = address[i];
+
+            byte[] outBytes = new byte[remoteAddrBytes.Length];
+            socket.IOControl(IOControlCode.RoutingInterfaceQuery, remoteAddrBytes, outBytes);
+            for (int i = 0; i < address.Size; i++) address[i] = outBytes[i];
+
+            EndPoint ep = remoteEndPoint.Create(address);
+            return (IPEndPoint)ep;
+        }
+
+        private static string QueryRoutingGatewayId(string gatewayIp)
+        {
+            foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+                foreach (UnicastIPAddressInformation ipa in ni.GetIPProperties().UnicastAddresses)
+                    if (ipa.Address.AddressFamily == AddressFamily.InterNetwork)
+                        if (ipa.Address.ToString() == gatewayIp)
+                            return ni.Name;
+            return string.Empty;
+        }
+
+        private static string QueryRoutingGatewayName(string gatewayId)
+        {
+            ManagementScope oMs = new ManagementScope();
+            ObjectQuery oQuery = new ObjectQuery($"Select * From Win32_NetworkAdapter WHERE NetConnectionID = \"{gatewayId}\"");
+            ManagementObjectSearcher oSearcher = new ManagementObjectSearcher(oMs, oQuery);
+            ManagementObjectCollection oReturnCollection = oSearcher.Get();
+            foreach (ManagementObject oReturn in oReturnCollection)
+                if (oReturn.Properties["ProductName"].Value != null)
+                    return oReturn.Properties["ProductName"].Value.ToString();
+            return string.Empty;
+        }
+
+        private static void PassIP(string ip)
+        {
+            Console.WriteLine($"{DateTime.Now}:       Checking Existing Route for {ip} ...");
+            
+            string cmd = $"print {ip}";
+            System.Diagnostics.ProcessStartInfo info = new System.Diagnostics.ProcessStartInfo("route", cmd);
+            info.CreateNoWindow = true;
+            info.RedirectStandardOutput = true;
+            info.RedirectStandardError = true;
+            info.UseShellExecute = false;
+
+            System.Diagnostics.Process p = System.Diagnostics.Process.Start(info);
+            p.Start();
+            string output = p.StandardOutput.ReadToEnd();
+            string error = p.StandardError.ReadToEnd();
+            if (!string.IsNullOrEmpty(error)) throw new Exception(error);
+
+            bool ex = false;
+            int vpnMetric = 0;
+            int dirMetric = 0;
+            string vpnGate = "";
+            string vpnMask = "255.255.255.255";
+
+            Regex rx = new Regex("[\\d\\.]+");
+            foreach (string d in output.Split(new char[] { '\r', '\n' }))
+                if (d.Trim().StartsWith(ip))
+                {
+                    MatchCollection mx = rx.Matches(d);
+                    if (mx[0].Value == ip)
+                    {
+                        if (mx[2].Value == defGateway)
+                        {
+                            ex = true;
+                            dirMetric = int.Parse(mx[4].Value);
+                        }
+                        else
+                        {
+                            vpnMetric = int.Parse(mx[4].Value);
+                            vpnMask = mx[1].Value;
+                            vpnGate = mx[2].Value;
+                        };
+                    };
+                };
+                                   
+            if(ex)
+            {
+                if (vpnMetric < dirMetric)
+                {
+                    preventNextSleep = true;
+                    Console.WriteLine($"{DateTime.Now}:         Metric found, rotating to Direct Gate: Direct={dirMetric}, {vpnName}={vpnMetric}");
+                    if (string.IsNullOrEmpty(vpnGate))
+                    {
+                        Console.WriteLine($"{DateTime.Now}:           Error: Gateway is bad `{vpnGate}`");
+                    }
+                    else
+                    {
+                        string res = SetRouteMetric(ip, vpnMask, vpnGate, "1000");
+                        Console.WriteLine($"{DateTime.Now}:           Status: {res}");
+                    };
+                }
+                else
+                {
+                    Console.WriteLine($"{DateTime.Now}:         Metric found, no need rotate: Direct={dirMetric}, {vpnName}={vpnMetric}\r\n");
+                };
+            }
+            else
+            {
+                preventNextSleep = true;
+                Console.WriteLine($"{DateTime.Now}:         Metric NOT found, adding route to Direct Gate");
+                string[] mm = ip.Split(new char[] { '.' });
+                for (int i = 0; i < mm.Length; i++) if (mm[i] != "0") mm[i] = "255"; else mm[i] = "0";
+                string mask = string.Join(".", mm);
+                string txt = AddStaticRoute(ip, mask);
+                Console.WriteLine($"{DateTime.Now}:         {txt}");                
+            };
+        }
+
+        private static string AddStaticRoute(string destination, string mask)
+        {
+            string cmd = $"add {destination} mask {mask} {defGateway} metric {defMetric}";
+            System.Diagnostics.ProcessStartInfo info = new System.Diagnostics.ProcessStartInfo("route", cmd);
+            info.CreateNoWindow = true;
+            info.RedirectStandardOutput = true;
+            info.RedirectStandardError = true;
+            info.UseShellExecute = false;
+
+            System.Diagnostics.Process p = System.Diagnostics.Process.Start(info);
+            p.Start();
+            string output = p.StandardOutput.ReadToEnd();
+            string error = p.StandardError.ReadToEnd();
+            return output + error;
+        }
+
+        private static string RemStaticRoute(string destination)
+        {
+            string cmd = $"delete {destination}";
+            System.Diagnostics.ProcessStartInfo info = new System.Diagnostics.ProcessStartInfo("route", cmd);
+            info.CreateNoWindow = true;
+            info.RedirectStandardOutput = true;
+            info.RedirectStandardError = true;
+            info.UseShellExecute = false;
+
+            System.Diagnostics.Process p = System.Diagnostics.Process.Start(info);
+            p.Start();
+            string output = p.StandardOutput.ReadToEnd();
+            string error = p.StandardError.ReadToEnd();
+            if (!string.IsNullOrEmpty(error)) preventNextSleep = true;
+            return output + error;
+        }
+
+        private static string SetRouteMetric(string ip, string mask, string gate, string metric)
+        {
+            string cmd = $"change {ip} mask {mask} {gate} metric {metric}";
+            System.Diagnostics.ProcessStartInfo info = new System.Diagnostics.ProcessStartInfo("route", cmd);
+            info.CreateNoWindow = true;
+            info.RedirectStandardOutput = true;
+            info.RedirectStandardError = true;
+            info.UseShellExecute = false;
+
+            System.Diagnostics.Process p = System.Diagnostics.Process.Start(info);
+            p.Start();
+            string output = p.StandardOutput.ReadToEnd();
+            string error = p.StandardError.ReadToEnd();
+            if (!string.IsNullOrEmpty(error)) throw new Exception(error);
+            return output;
+        }
+
+        private static void NeedIP(string ip)
+        {
+            Console.WriteLine($"{DateTime.Now}:       Checking Existing Route for {ip} ...");
+
+            string cmd = $"print {ip}";
+            System.Diagnostics.ProcessStartInfo info = new System.Diagnostics.ProcessStartInfo("route", cmd);
+            info.CreateNoWindow = true;
+            info.RedirectStandardOutput = true;
+            info.RedirectStandardError = true;
+            info.UseShellExecute = false;
+
+            System.Diagnostics.Process p = System.Diagnostics.Process.Start(info);
+            p.Start();
+            string output = p.StandardOutput.ReadToEnd();
+            string error = p.StandardError.ReadToEnd();
+            if (!string.IsNullOrEmpty(error)) throw new Exception(error);
+
+            int count = 0;
+            bool ex = false;
+
+            Regex rx = new Regex("[\\d\\.]+");
+            foreach (string d in output.Split(new char[] { '\r', '\n' }))
+                if (d.Trim().StartsWith(ip))
+                {
+                    count++;
+                    MatchCollection mx = rx.Matches(d);
+                    if (mx[0].Value == ip) ex = true;
+                };
+            if (count == 0)
+                Console.WriteLine($"{DateTime.Now}:       No any routes for {ip}\r\n");
+            else if (count == 1 && ex)
+            {
+                Console.WriteLine($"{DateTime.Now}:       Found not need Route for {ip}, deleting ...");
+                string res = RemStaticRoute(ip);
+                Console.WriteLine($"{DateTime.Now}:           Status: {res}\r\n");
+            }
+            else Console.WriteLine($"{DateTime.Now}:           No need changes to IP Route Table\r\n");
+        }
+
+        private static void PrintIP()
+        {
+            string cmd = $"print";
+            System.Diagnostics.ProcessStartInfo info = new System.Diagnostics.ProcessStartInfo("route", cmd);
+            info.CreateNoWindow = true;
+            info.RedirectStandardOutput = true;
+            info.RedirectStandardError = true;
+            info.UseShellExecute = false;
+
+            System.Diagnostics.Process p = System.Diagnostics.Process.Start(info);
+            p.Start();
+            string output = p.StandardOutput.ReadToEnd();
+
+            Console.WriteLine("-------------------------- IP TABLE --------------------------");
+            Regex rx = new Regex("[\\d\\.]+");
+            foreach (string d in output.Split(new char[] { '\r', '\n' }))
+            {
+                MatchCollection mx = rx.Matches(d);
+                if (d.Contains(".") && mx.Count == 4 && (!d.Contains(":")))
+                    Console.WriteLine(d.Trim());
+            };
+            Console.WriteLine("--------------------------------------------------------------");
+        }
+    }
+}
